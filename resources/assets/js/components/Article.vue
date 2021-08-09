4586b7c977627b1bd73bb28d14ee28652c2012c5
ShkAdnan <AdnanAsad@outlook.com>
2021-08-09 21:15:59 +0500

    First Commit

---
+++ resources/assets/js/components/Article.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <h2>Articles</h2>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-bind:class="[{ disabled: !pagination.prev_page_url }]" class="page-item">
+                    <a class="page-link" @click="fetchArticle(pagination.prev_page_url)" href="#">Previous</a>
+                </li>
+
+                <li class="page-item disabled">
+                    <a class="page-link text-dark" href="#">{{ pagination.current_page }} of {{ pagination.last_page }}</a>
+                </li>
+
+                <li v-bind:class="[{ disabled: !pagination.next_page_url }]" class="page-item">
+                    <a class="page-link" @click="fetchArticle(pagination.next_page_url)" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="card card-body" v-for="article in articles" :key="article.id">
+            <h3>{{ article.title}}</h3>
+            <p> {{ article.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default({
+    data(){
+        return{
+            articles : [],
+            article : {
+                id    : '',
+                title : '',
+                body  : ''
+            },
+            article_id : '',
+            pagination : {},
+            edit       : false
+        }
+    },
+    created(){
+        this.fetchArticle();
+    },
+    methods : {
+
+        fetchArticle(page_url){
+            let vm = this;
+            page_url = page_url || 'api/articles'
+            fetch(page_url)
+            .then(res => res.json() )
+            .then( res => {
+                this.articles = res.data
+                vm.makePaginations( res.meta , res.links ) 
+            })
+            .catch(err => console.log( "Error" ) )
+        },
+
+        makePaginations( meta , links ){
+            let pagination = {
+                current_page  : meta.current_page,
+                last_page     : meta.last_page,
+                next_page_url : links.next,
+                prev_page_url : links.prev
+            }
+
+            this.pagination = pagination;
+        }
+    }
+})
+
+</script>
---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand">
+                Article
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default ({
+
+})
+</script>
